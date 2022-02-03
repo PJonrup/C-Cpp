d3ee72c1169d48a03a567cc25629771efdce06f4
Pontus Jönrup <ponjo823@student.liu.se>
2022-02-03 15:41:50 +0100

    done with chopping wood

--- TDDD95/Cpp/2_exercise_2/chop.cpp
+++ TDDD95/Cpp/2_exercise_2/chop.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <algorithm>
-#include <unordered_map>
+#include <map>
 #include <set>
 using namespace std;
 
@@ -16,31 +16,28 @@ int main() {
     int last = n+1;
     bool err = true;
 
-    // Get the input.
+    // Get the input set up v in an map.
     unsigned int *arr = new unsigned int[n];
-    unordered_map<int, int> umap;
+    map<int, int> mp;
 
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
-    }
 
-    // Set up v in an unordered map (reversed as map adds elements to the front)
-    for (int i = n - 1; i >= 0; i--) {
         if (arr[i] == last) {
             err = false;
         }
 
-        if (umap.count(arr[i]) >= 1) {
-            umap[arr[i]]++;
+        if (mp.count(arr[i]) >= 1) {
+            mp[arr[i]] += 1;
         }
         else {
-            umap[arr[i]] = 1;
+            mp[arr[i]] = 1;
         }
-    }   
+    }
 
     // Check so that we have found the last node in v.
     if (err == true) {
-        cout << "Error" << endl;
+        cout << "Error\n";
         return 0;
     }
 
@@ -48,24 +45,20 @@ int main() {
     set<int> leafs;
 
     for (int i = 0; i < n; i++) {
-        if (umap.count(i + 1) == 0) {
+        if (mp.count(i + 1) == 0) {
             leafs.insert(i + 1);
         }
     }
 
     // Loops though n (v).
     for (int i = 0; i < n; i++) {
-        // Loops through second element of map and prints leaf-nodes.
-        for (int j = 0; j < umap.begin()->second; j++) {
-            //cout << *(leafs.begin()) << '\n';
+        if (mp[arr[i]] > 0) {
             cout << *(leafs.begin()) << endl;
-            leafs.erase(*(leafs.begin()));
-            // Checks if we are on the last iteration of the second element of the map,
-            // removes it from the map and adds it to leafs (leaf-nodes).
-            if (j + 1 == umap.begin()->second) {
-                leafs.insert(umap.begin()->first);
-                umap.erase(umap.begin());
-                break; 
+            leafs.erase(*leafs.begin());
+            mp[arr[i]]--;
+            if (mp[arr[i]] == 0) {
+                leafs.insert(arr[i]);
+                mp.erase(mp[arr[i]]);
             }
         }
     }
